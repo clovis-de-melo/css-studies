5476849ca86dfb8260b0c98d93fe07b9d158480d
Clovis de Melo <106097988+clovis-de-melo@users.noreply.github.com>
2025-03-09 22:18:09 -0300

    feat: test show and hide function

--- javascript/nav-bar.js
+++ javascript/nav-bar.js
@@ -1,1 +1,29 @@
-console.log("test")
+// console.log("test")
+
+function menuShow() {
+    let menuMobile = document.querySelector(".mobile-menu");
+    if (menuMobile.classList.contains("open")) {
+        menuMobile.classList.remove("open");
+        document.querySelector(".icon").src = "/src/svg/menu_white_36dp.svg";
+    } else {
+        menuMobile.classList.add("open");
+        document.querySelector(".icon").src = "/src/svg/close_white_36dp.svg";
+    }
+}
+
+/* function hide() {
+    alert("hide content function is working");
+}
+
+
+function show() {
+    alert("show content function is working");
+} */
+
+function hide(el) {
+    document.getElementById(el).style.display = "none";
+}
+
+function show(el) {
+    document.getElementById(el).style.display = "block";
+}
